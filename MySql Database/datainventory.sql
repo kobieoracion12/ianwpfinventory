a6bb9441483ec727fe3a8899289dd19600064f3c
kobieoracion12 <kobie.oracion12@gmail.com>
2020-11-17 21:27:02 +0800

    Update for datainventory Table in MySql

--- MySql Database/datainventory.sql
+++ MySql Database/datainventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2020 at 08:11 AM
+-- Generation Time: Nov 17, 2020 at 02:25 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -43,12 +43,12 @@ CREATE TABLE `datainventory` (
 --
 
 INSERT INTO `datainventory` (`prodNo`, `prodItem`, `prodBrand`, `prodQty`, `prodSRP`, `prodRP`, `prodDOA`, `prodEXPD`) VALUES
-(48025522, 'Magic Sarap 8g', 'Maggi', 0, 37, 48, '2020-11-09', '2020-11-12'),
-(750515018402, 'Skyflakes Crackers 25g', 'Monde MY San Corporation', 0, 50, 60, '2020-03-23', '2021-03-23'),
-(4800016022180, 'Great Taste White Crema', 'Universal Robina', 0, 102, 110, '2020-06-14', '2021-06-14'),
-(4800016043093, 'Nips White Chocolate', 'Jack n Jill', 0, 24, 26, '2020-09-18', '2021-09-18'),
-(4800016306013, 'Maxx Honey Lemon', 'Jack n Jill', 0, 35, 50, '2020-08-28', '2021-02-28'),
-(4800016622533, 'V Cut Spicy Barbeque Flavor', 'Jack n Jill', 0, 12, 13, '2020-09-05', '2021-02-05'),
+(48025522, 'Magic Sarap 8g', 'Maggi', 10, 37, 48, '2020-11-17', '2020-11-29'),
+(750515018402, 'Skyflakes Crackers 25g', 'Monde MY San Corporation', 100, 50, 60, '2020-11-17', '2021-03-23'),
+(4800016022180, 'Great Taste White Crema', 'Universal Robina', 20, 102, 110, '2020-11-17', '2020-11-22'),
+(4800016043093, 'Nips White Chocolate', 'Jack n Jill', 15, 24, 26, '2020-11-17', '2021-09-18'),
+(4800016306013, 'Maxx Honey Lemon', 'Jack n Jill', 100, 35, 50, '2020-11-17', '2021-02-28'),
+(4800016622533, 'V Cut Spicy Barbeque Flavor', 'Jack n Jill', 20, 12, 13, '2020-11-17', '2021-02-05'),
 (4800016644801, 'Piattos Cheese', 'Jack n Jill', 0, 12, 13, '2020-09-12', '2021-03-12'),
 (4800016671807, 'Piattos Sour Cream & Onion', 'Jack n Jill', 0, 12, 13, '2020-02-07', '2020-09-07'),
 (4800092113338, 'Rebisco Crackers', 'Republic Biscuit Corporation', 0, 50, 60, '2020-07-12', '2021-07-12'),
