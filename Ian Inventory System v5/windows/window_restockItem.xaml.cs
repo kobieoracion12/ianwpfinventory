00bcf8ac8069dbc7298776fc5bdf48a4b7130a7b
kobieoracion12 <kobie.oracion12@gmail.com>
2020-11-17 17:03:56 +0800

    Restock Functions & Some Design Updates

--- Ian Inventory System v5/windows/window_restockItem.xaml.cs
+++ Ian Inventory System v5/windows/window_restockItem.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
+using NavigationDrawerPopUpMenu2.classes;
 
 namespace NavigationDrawerPopUpMenu2.windows
 {
@@ -19,9 +22,37 @@ namespace NavigationDrawerPopUpMenu2.windows
     /// </summary>
     public partial class window_restockItem : Window
     {
+        MySqlConnection con = new MySqlConnection("server=127.0.0.1;user id=ianinventory;database=iantestinventory; password='C73DPJxyXICd4Mjq'");
         public window_restockItem()
         {
             InitializeComponent();
+        }
+
+        private void entrySearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (entrySearch.Text.Length > 0)
+            {
+                string search = entrySearch.Text;
+
+                DataTable dt = new DataTable();
+                con.Open();
+                MySqlDataReader myReader = null;
+                MySqlCommand myCommand = new MySqlCommand("SELECT * FROM datainventory WHERE prodNo= '" + search + "'", con);
+
+                myReader = myCommand.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    restockItem.Text = (myReader["prodItem"].ToString());
+                    restockBrand.Text = (myReader["prodBrand"].ToString());
+                    onrestockQty.Text = (myReader["prodQty"].ToString());
+                    restockSRP.Text = (myReader["prodSRP"].ToString());
+                    restockRP.Text = (myReader["prodRP"].ToString());
+                    restockEXPD.Text = (myReader["prodEXPD"].ToString());
+
+                }
+                con.Close();
+            }
         }
     }
 }
