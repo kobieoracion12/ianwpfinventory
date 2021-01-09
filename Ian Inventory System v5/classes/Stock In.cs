8d6ef96d211fe5e280d7ea6dc0f64d75bed35a95
mirasrobert <mirasrobertgaming@yahoo.com>
2021-01-09 14:01:10 +0800

    reset data in stock in w/ export

---
+++ Ian Inventory System v5/classes/Stock In.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationDrawerPopUpMenu2.classes
+{
+    class Stock_In
+    {
+        public string stockinRefNo { get; set; }
+        public string stockinPcode  { get; set; }
+        public string stockinItem { get; set; }
+        public string stockinQty { get; set; }
+        public string stockinPrice { get; set; }
+        public string stockinDate { get; set; }
+        public string stockinId { get; set; }
+        public string stockinStatus { get; set; }
+    }
+}
--- Ian Inventory System v5/usercontrols/usc_stock_history.xaml.cs
+++ Ian Inventory System v5/usercontrols/usc_stock_history.xaml.cs
@@ -17,12 +17,16 @@ using System.Data;
 using NavigationDrawerPopUpMenu2.classes;
 using NavigationDrawerPopUpMenu2.windows;
 using NavigationDrawerPopUpMenu2.reports;
+using Microsoft.Win32;
+using Microsoft.Office.Interop.Excel;
 
 namespace NavigationDrawerPopUpMenu2.usercontrols
 {
     public partial class usc_stock_history : UserControl
     {
         Database conn = new Database();
+        Stock_In  stockout = new Stock_In();
+        List<Stock_In> myStockin = new List<Stock_In>();
         string reportToPrint = "Select";
         public usc_stock_history()
         {
@@ -47,7 +51,7 @@ namespace NavigationDrawerPopUpMenu2.usercontrols
                 // Adapter
                 MySqlDataAdapter adapter = conn.adapter();
                 //  Datatable
-                DataTable dt = new DataTable("stock_in");
+                System.Data.DataTable dt = new System.Data.DataTable("stock_in");
                 // Fill the datatable
                 adapter.Fill(dt);
                 listViewRecords.ItemsSource = dt.DefaultView;
@@ -254,5 +258,137 @@ namespace NavigationDrawerPopUpMenu2.usercontrols
             rptstockout.printPreview();
             rptstockout.ShowDialog();
         }
+
+        // Export Data to Excel
+        private void exportDataToExcel()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel WorkBook|*.xls";
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); // Save to MyDocuments
+            try
+            {
+                if (sfd.ShowDialog() == true)
+                {
+                    Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                    Workbook wb = app.Workbooks.Add(XlSheetType.xlWorksheet);
+                    Worksheet ws = (Worksheet)app.ActiveSheet;
+                    app.Visible = false;
+                    ws.Cells[1, 1] = "Stock In #";
+                    ws.Cells[1, 2] = "Item No";
+                    ws.Cells[1, 3] = "Item";
+                    //ws.Cells[1, 4] = "Date of Purchase";
+                    ws.Cells[1, 4] = "Quantity";
+                    ws.Cells[1, 5] = "Price List";
+                    ws.Cells[1, 6] = "Date";
+                    int i = 2;
+
+                    // Start a Query
+                    string sql = "SELECT * FROM stock_in";
+                    conn.query(sql);
+                    conn.Open();
+                    MySqlDataReader reader = conn.read();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string stockoutTransNo = reader["stockinRefNo"].ToString();
+                            string stockoutNo = reader["stockinPcode"].ToString();
+                            string stockoutItem = reader["stockinItem"].ToString();
+                            string stockoutQty = reader["stockinQty"].ToString();
+                            string stockoutPrice = reader["stockinPrice"].ToString();
+                            string stockoutDate = reader["stockinDate"].ToString();
+
+
+                            myStockin.Add(new Stock_In { stockinRefNo = stockoutTransNo, stockinPcode = stockoutNo, stockinItem = stockoutItem, stockinQty = stockoutQty, stockinPrice = stockoutPrice, stockinDate = stockoutDate });
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No Rows", "Notice", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                    }
+
+                    reader.Close();
+                    reader.Dispose();
+                    conn.Close();
+
+                    // End of Query
+
+
+                    //string das = listViewSales.Items[0].ToString();
+                    foreach (Stock_In item in myStockin)
+                    {
+                        //ws.Cells[i, 1] = (string)((DataRowView)listViewSales.SelectedItems[0])["refNo"];
+                        ws.Cells[i, 1] = item.stockinRefNo;
+                        ws.Cells[i, 2] = item.stockinPcode;
+                        ws.Cells[i, 3] = item.stockinItem;
+                        ws.Cells[i, 4] = item.stockinQty;
+                        ws.Cells[i, 5] = item.stockinPrice;
+                        ws.Cells[i, 6] = item.stockinDate;
+
+                        i++;
+
+                    } // Closing of Foreach
+
+                    wb.SaveAs(sfd.FileName, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+                    app.Quit();
+                    myStockin.Clear(); // Clear All Items
+                    MessageBox.Show("Your data has been successfully exported", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                } // Closing of IF Statement
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        // Delete All From Database
+        private void ClearAllDataFromDatabase()
+        {
+            string sql = "DELETE FROM stock_in";
+            conn.query(sql);
+
+            try
+            {
+                conn.Open();
+                conn.cmd().Prepare();
+                conn.execute();
+                MessageBox.Show("All data has been removed successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        // Reset Button
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult msg = MessageBox.Show("Do you want to export all the data before clearing them all?", "Confirmation", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (msg == MessageBoxResult.Yes)
+            {
+                // If Yes then Delete the Data
+                exportDataToExcel();
+                ClearAllDataFromDatabase();
+                loadDataForRecord();
+            }
+            else if (msg == MessageBoxResult.No)
+            {
+                MessageBoxResult deleteMsg = MessageBox.Show("This will clear all the data without exporting a file, Are you sure?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (deleteMsg == MessageBoxResult.Yes)
+                {
+                    // Delete the Data
+                    ClearAllDataFromDatabase();
+                    loadDataForRecord();
+                }
+            }
+            else if (msg == MessageBoxResult.Cancel)
+            {
+                return;
+            }
+            else { return; }
+        }
+
     }
 }
